063e6a55a4875cddfd4f033062b595d2d5ba1f44
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2020-10-27 16:57:49 -0300

    Configurando o app na porta 3000.

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,13 @@ app.use(express.json());
 
 const port = 3000;
 
+const sorteioService = require('./service/SorteioService');
+
 app.get('/sortear', (req, res) => {
-    res.send("Servidor Ativo!")
+    const resultado = sorteioService.execute();
+    res.json(resultado).send();
+})
+
+app.listen(port, () => {
+    console.log(`Servidor Ativo! na porta: ${port}`)
 })
