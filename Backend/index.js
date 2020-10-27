82cb363e90ded58f5dbaea9af8c2ad08bf502474
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2020-10-27 19:40:12 -0300

    Adcionada rota '/lerArquivo'

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { request } = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 app.use(cors());
@@ -10,9 +11,17 @@ const port = 3000;
 
 const sorteioService = require('./service/SorteioService');
 
+const leitorArquivoService = require('./service/LeitorArquivoService');
+const upload = multer(require('./config/upload'));
+
 app.get('/sortear', (req, res) => {
     const resultado = sorteioService.execute();
     res.json(resultado).send();
+})
+
+app.post('/lerArquivo', upload.single('file'), async (req, res) => {
+    const conteudoArquivo = await leitorArquivoService.execute(req.file.path);
+    res.json(conteudoArquivo).send();
 })
 
 app.listen(port, () => {
